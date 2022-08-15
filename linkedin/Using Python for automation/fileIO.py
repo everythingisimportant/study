ba66bd2feb4fd8d00f651c356952f52b6f92b793
nnguyenminh <minhnhatt716@gmail.com>
2022-08-15 16:51:56 +0700

    how to read file, write file, execute terminal cmd

---
+++ linkedin/Using Python for automation/fileIO.py
@@ -0,0 +1,20 @@
+
+# Read file
+# f = open('inputFile.txt', 'r')
+# count = 0
+# for line in f:
+#     print(str(count) + line)
+#     count = count + 1
+# f.close()
+
+# Write file
+# f = open('inputFile.txt', 'r')
+# newFile = open('newFile.txt', 'w')
+# for line in f:
+#     line_split = line.split()
+#     if line_split[2] == 'P':
+#         newFile.write(line)
+# f.close()
+# newFile.close()
+
+print('Hello')
---
+++ linkedin/Using Python for automation/script.py
@@ -0,0 +1,4 @@
+# Executing terminal cmd
+import subprocess
+for i in range(0,5):
+    subprocess.check_call(['python', 'fileIO.py'])
